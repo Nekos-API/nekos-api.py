09c029cdfc8f1286e948a821b14d18b7e28eafe3
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-06-07 01:28:03 -0300

    Added `Category` to`__init__.py`

--- nekosapi/__init__.py
+++ nekosapi/__init__.py
@@ -2,4 +2,4 @@
 Documentation: https://nekosapi.com/docs/libraries/python/
 API documentation: https://nekosapi.com/docs/
 """
-from .resources import Resource, Image, User
+from .resources import Resource, Image, User, Category
